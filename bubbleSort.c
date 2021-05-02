15591fb4d3fb11dc635580bc3942944060fc09eb
trangdlampf <42576605+trangdlampf@users.noreply.github.com>
2021-05-02 16:30:32 -0400

    bubbleSort, not tested
    bubbleSort, not tested

---
+++ bubbleSort.c
@@ -0,0 +1,17 @@
+// not tested yet
+const bubbleSort = function(array) {
+  let swaps;
+  do {
+    swaps = false;
+    for (let i = 0; i < array.length - 1; i++) {
+      if (array[i] > array[i + 1]) {
+        let temp = array[i + 1];
+        array[i + 1] = array[i];
+        array[i] = temp;
+        swaps = true;
+      }
+    }
+  } while (swaps);
+  
+  return array;
+};
