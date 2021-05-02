51940c24d762404704812b88da1bebd39d20a123
trangdlampf <42576605+trangdlampf@users.noreply.github.com>
2021-05-01 20:42:28 -0400

    2 ways: reverse_str, inside and outside of main()
    2 ways: reverse_str, inside and outside of main()

--- reverse_str_Cpp.c
+++ reverse_str_Cpp.c
@@ -1,20 +1,51 @@
+// FIRST WAY
 #include <iostream>
 using namespace std;
 
 int main() {
  
-  string greeting = "Hello";
-  int len = greeting.length(); 	// get length of string
-  int n = len - 1;					    // subtract 1 to get index
+  string myStr1 = "Hello";
+  int len = myStr1.length(); 	// get length of string
+  int n = len - 1;		// subtract 1 to get index
   
   // loop through the string
-  for(int i=0; i<(len/2); i++){ // i loops from h to o in hello
+  for(int i=0; i<(len/2); i++){
 	  
     // Use swap method to switch values at each index
-    swap(greeting[i],greeting[n]);
+    swap(myStr1[i],myStr1[n]);
 	
-	// Decrement because I am going from letter o to h in hellow
+    // Decrement because I am going from letter o to h in hello
     n = n - 1;
   }
-  cout << greeting << endl; // prints: olleh 
+  cout << myStr1 << endl; // print
+}
+
+///////////////////////////////////////////
+// SECOND WAY
+// DOES SAME: function taken out of main()
+#include <iostream>
+using namespace std;
+
+void reverseStr(string& myinputstr) {
+	
+    int len = myinputstr.length(); 	// get length of string
+    int n = len - 1;			// subtract 1 to get index
+	
+    // loop through the string
+    for(int i=0; i<(len/2); i++){
+	  
+	//Using the swap method to switch values at each index
+	swap(myinputstr[i],myinputstr[n]);
+		
+	// Decrement because I am going from letter o to h in hellow
+	n = n - 1;
+    }		
+}
+
+int main() { 
+
+	string myStr1 = "Hello";
+	reverseStr(myStr1);
+	cout << myStr1;
+		
 }
