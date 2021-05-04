1a0f625b20f7d7d31bb2ceaa3938d77e672ca696
trangdlampf <42576605+trangdlampf@users.noreply.github.com>
2021-05-04 09:46:56 -0400

    Questions and Visualization of Sort
    Questions and Visualization of Sort, description of

---
+++ topQuest_and_Vis.c
@@ -0,0 +1,24 @@
+// Visualizations of different Sorts: 
+// https://www.cs.usfca.edu/~galles/visualization/Algorithms.html
+
+// https://www.educative.io/blog/top-cpp-coding-interview-questions
+
+/*  Summary of Topic
+
+    Big O Notation
+    Data Structures
+    Recursion
+    Dynamic Programming
+    More practice questions
+    Wrapping up and next steps
+*/
+
+/* 
+    Move Zero to beginning of array
+    Reverse Even Nodes in a Linked List
+    Sort a Linked List using Merge sort
+    Count of Palidrome Substring: for Montana: is ana so Count is 3 
+    Reverse Word in Sentence
+    Number of Vowels in String (18)
+
+*/
